f9df8126ca48c192638028101959e9968c7ab5d5
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-09-06 13:52:25 +0300

    Successfully mapped to external API for data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import addFavicon from './modules/addFavicon';
+import getMedia from './modules/getMedia';
 import './styles/styles.css';
 
-window.onload = () => {
+const baseUrl = process.env.BASE_URL;
+const apiKey = process.env.API_KEY;
+const url = `${baseUrl}/trending/all/day?api_key=${apiKey}`;
+
+window.onload = async () => {
   addFavicon();
+  const mediaList = await getMedia(url);
+  console.log(mediaList);
 };
