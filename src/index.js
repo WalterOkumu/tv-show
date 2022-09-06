b9db5fd5928ac0444c513f85ce7154b804d1a5e4
Alier Philip <alierditmaguet@gmail.com>
2022-09-06 20:31:41 +0200

    Add media query Added media query to make the header section responsive fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import addFavicon from './modules/addFavicon';
 import './styles/styles.css';
+import logo from './assets/faviconio-logo/logo.svg';
+
+document.querySelector('.logo').src = logo;
 
 window.onload = () => {
   addFavicon();
